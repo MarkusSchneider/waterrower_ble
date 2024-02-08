ef27d6cb4f080cae2a26424456ffa8cc829b8eea
SchneiderMK <m.schneider@online.de>
2024-02-08 15:35:50 +0000

    added recording

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,25 @@
-import http from 'http';
+// import http from 'http';
 
-export const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-  res.end(
-    JSON.stringify({ data: 'It Works!' })
-  );
-});
+import { WaterRower } from './waterrower-serial/waterrower-serial';
 
-server.listen(3000, () => {
-  console.log('Server running on http://localhost:3000/');
-});
+// export const server = http.createServer((req, res) => {
+//   res.writeHead(200, { 'Content-Type': 'application/json' });
+//   res.end(
+//     JSON.stringify({ data: 'It Works!' })
+//   );
+// });
 
+// server.listen(3000, () => {
+//   console.log('Server running on http://localhost:3000/');
+// });
+try {
+  const waterrower = new WaterRower();
+
+  waterrower.on('initialized', () => {
+    waterrower.reset();
+    waterrower.startRecording('./recording.txt');
+  });
+
+} catch (error) {
+  console.log(error);
+}
